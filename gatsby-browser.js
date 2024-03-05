e3d7ab1fe6d25bea9724aeee559a45136e95884d
willlee17 <williamklee1996@gmail.com>
2024-03-04 21:10:02 -0800

    UUID

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { GrowthBook, GrowthBookProvider } from '@growthbook/growthbook-react'
 
+const create_UUID = () => {
+    let dt = new Date().getTime();
+  
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+       let r = (dt + Math.random() * 16) % 16 | 0;
+       dt = Math.floor(dt / 16);
+       return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
+
 let visitor_id = localStorage.getItem("growthbook_visitor_id");
 
 if (!visitor_id) {
